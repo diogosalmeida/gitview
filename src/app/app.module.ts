0258f6ce9005e11c1a00c5bd6d60ac23af53c1c9
diogosalmeida <diogomean@gmail.com>
2018-02-14 00:11:58 -0200

    fix and implements logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderSearchComponent } from './header-search/header-search.component';
+import { InfoUserComponent } from './info-user/info-user.component';
+import { ServiceUserInfo } from './service-user-info.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSearchComponent,
+    InfoUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServiceUserInfo],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
